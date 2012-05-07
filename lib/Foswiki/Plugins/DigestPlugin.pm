110630acb53e8adbb93d9a9fb2bc6f011969de77
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-07 09:39:48 +0000

    Item11808: reverting perltidy
    git-svn-id: http://svn.foswiki.org/trunk/DigestPlugin@14707 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/DigestPlugin.pm
+++ lib/Foswiki/Plugins/DigestPlugin.pm
@@ -13,99 +13,96 @@
 # GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 
+
 package Foswiki::Plugins::DigestPlugin;
 
 use strict;
-require Foswiki::Func;       # The plugins API
-require Foswiki::Plugins;    # For the API version
+require Foswiki::Func;    # The plugins API
+require Foswiki::Plugins; # For the API version
 
 use vars qw(
   $VERSION $RELEASE $SHORTDESCRIPTION $NO_PREFS_IN_TOPIC
   $baseWeb $baseTopic $factory
 );
 
-$VERSION           = '$Rev$';
-$RELEASE           = '1.0';
-$SHORTDESCRIPTION  = 'Calculate a message digest, i.e. MD5';
+$VERSION = '$Rev$';
+$RELEASE = '1.0';
+$SHORTDESCRIPTION = 'Calculate a message digest, i.e. MD5';
 $NO_PREFS_IN_TOPIC = 1;
 
-use constant DEBUG => 0;     # toggle me
+use constant DEBUG => 0; # toggle me
 
 ###############################################################################
 sub writeDebug {
-    return unless DEBUG;
-    print STDERR "- DigestPlugin - " . $_[0] . "\n";
-
-    #Foswiki::Func::writeDebug("- DigestPlugin - $_[0]");
+  return unless DEBUG;
+  print STDERR "- DigestPlugin - " . $_[0] . "\n";
+  #Foswiki::Func::writeDebug("- DigestPlugin - $_[0]");
 }
+
 
 ###############################################################################
 sub initPlugin {
-    ( $baseTopic, $baseWeb ) = @_;
+  ($baseTopic, $baseWeb) = @_;
 
-    Foswiki::Func::registerTagHandler( 'DIGEST', \&handleDIGEST );
-    $factory = undef;
+  Foswiki::Func::registerTagHandler('DIGEST', \&handleDIGEST);
+  $factory = undef;
 
-    return 1;
+  return 1;
 }
 
 ###############################################################################
 sub handleDIGEST {
-    my ( $session, $params, $topic, $web ) = @_;
+  my ($session, $params, $topic, $web) = @_;
 
-    my $data   = $params->{_DEFAULT} || '';
-    my $type   = $params->{type}     || 'MD5';
-    my $output = $params->{output}   || 'hex';
+  my $data = $params->{_DEFAULT} || '';
+  my $type = $params->{type} || 'MD5';
+  my $output = $params->{output} || 'hex';
 
-    my $digest;
-    unless ($factory) {
-        require Digest;
-        $factory = Digest->new($type);
-    }
+  my $digest;
+  unless ($factory) {
+    require Digest;
+    $factory = Digest->new($type);
+  }
 
-    $data = Foswiki::Func::expandCommonVariables( $data, $topic, $web )
-      if expandVariables( \$data, $web, $topic );
+  $data = Foswiki::Func::expandCommonVariables($data, $topic, $web)
+    if expandVariables(\$data, $web, $topic);
 
-    $factory->add($data);
+  $factory->add($data);
 
-    if ( $output eq 'hex' ) {
-        $digest = $factory->hexdigest;
-    }
-    elsif ( $output eq 'b64' ) {
-        $digest = $factory->b64digest;
-    }
-    elsif ( $output eq 'binary' ) {
-        $digest = $factory->digest;
-    }
-    else {
-        $digest = $factory->hexdigest;
-    }
+  if ($output eq 'hex') {
+    $digest = $factory->hexdigest;
+  } elsif ($output eq 'b64') {
+    $digest = $factory->b64digest;
+  } elsif ($output eq 'binary') {
+    $digest = $factory->digest;
+  } else {
+    $digest = $factory->hexdigest;
+  }
 
-    #writeDebug("digest of '$data' is '$digest'");
+  #writeDebug("digest of '$data' is '$digest'");
 
-    return $digest;
+  return $digest;
 }
 
 ###############################################################################
 sub expandVariables {
-    my ( $text, $web, $topic, %params ) = @_;
+  my ($text, $web, $topic, %params) = @_;
 
-    my $found = 0;
-
-    foreach my $key ( keys %params ) {
-        if ( $$text =~ s/\$$key\b/$params{$key}/g ) {
-            $found = 1;
-
-            #writeDebug("expanding $key->$params{$key}");
-        }
+  my $found = 0;
+  
+  foreach my $key (keys %params) {
+    if($$text =~ s/\$$key\b/$params{$key}/g) {
+      $found = 1;
+      #writeDebug("expanding $key->$params{$key}");
     }
-    $found = 1 if $$text =~ s/\$percnt/\%/go;
-    $found = 1 if $$text =~ s/\$nop//g;
-    $found = 1 if $$text =~ s/\$n/\n/go;
-    $found = 1 if $$text =~ s/\$t\b/\t/go;
-    $found = 1 if $$text =~ s/\$dollar/\$/go;
+  }
+  $found = 1 if $$text =~ s/\$percnt/\%/go;
+  $found = 1 if $$text =~ s/\$nop//g;
+  $found = 1 if $$text =~ s/\$n/\n/go;
+  $found = 1 if $$text =~ s/\$t\b/\t/go;
+  $found = 1 if $$text =~ s/\$dollar/\$/go;
 
-    return $found;
+  return $found;
 }
 
 1;
